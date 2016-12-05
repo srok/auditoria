ef77bcf7dfe768be6e6c7a657a2ecadbe0136380
Rodrigo Barrera <Rodrigo Barrera>
2016-12-05 16:39:24 -0300

    test app 3

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,136 +1,110 @@
-angular.module('starter.controllers', [])
+angular.module('ionic-camera.controllers', [])
 
-.controller('DashCtrl', function($scope, backandService, logService) {
+.controller('HomeCtrl', ['$scope', "CameraPopover", "$ionicActionSheet",function($scope, CameraPopover, $ionicActionSheet){
+    $scope.showProgress = false;
 
-  var log = logService.log;
-  log('DashCtrl init');
+  var uploadFileUrl = "your serve api";
 
-  var counter = 0;
-  $scope.allItems = [];
-  $scope.item = {
-    name: 'hello world'
-  };
-  $scope.message = "hello World";
-
-  // will run on init
-  function init() {
-    $scope.itemName = '';
-    backandService.getTodos().then(function(d) {
-      log('list received', d);
-      $scope.allItems = d.data.data;
-    });
-  }
-
-  $scope.addItem = function() {
-    var txt = $scope.item.name;
-    log(txt)
-    backandService.addTodo({
-      name: txt + '-' + counter++
-    }).then(function(d) {
-      log('object added: ', d);
-      init();
-    });
-  }
-
-  init();
-})
-
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
-})
-
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
-
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-})
-
-.controller('PluginsCtrl', function($scope, logService, /*$cordovaCamera,*/ $cordovaGeolocation) {
-
-  var log = logService.log;
-  log('PluginsCtrl init');
-
-  // defaults for camera
-  $scope.picture = null;
-  $scope.cameraOptions = {
-    quality: 50,
-    /*destinationType: Camera.DestinationType.DATA_URL,
-    sourceType: Camera.PictureSourceType.CAMERA,*/
-    allowEdit: true,
-    //encodingType: Camera.EncodingType.JPEG,
-    targetWidth: 800,
-    targetHeight: 600,
-    //popoverOptions: CameraPopoverOptions,
-    saveToPhotoAlbum: false,
-    correctOrientation: true
-  };
-
-  // defaults for position
-  $scope.posOptions = {
-    timeout: 5000,
-    enableHighAccuracy: true
-  };
-
-
-  $scope.pictureClick = function() {
-    log('pictureClick()');
-
-    //$cordovaCamera
-    navigator.camera.getPicture($scope.cameraOptions).then(function(imageData) {
-      var image = document.getElementById('cameraImage');
-      image.src = "data:image/jpeg;base64," + imageData;
-    }, function(err) {
-      // error
+  $scope.showActionSheet = function(){
+    // Show the action sheet
+    var hideSheet = $ionicActionSheet.show({
+      buttons: [
+        { text: 'Take Photo' },
+        { text: 'Take Phone from albums' }
+      ],
+      // destructiveText: 'Delete',
+      titleText: 'Select photos from',
+      cancelText: 'Cancel',
+      cancel: function() {
+        hideSheet();
+      },
+      buttonClicked: function(index) {
+        // click "take phone"
+        if(index == 0){
+          takePicture({
+            quality : 100,
+            allowEdit : true,
+            targetWidth: 500,
+            targetHeight: 225,
+            // Android doesn't recognize this.
+            // http://stackoverflow.com/questions/29392639/error-capturing-image-with-phonegap-on-android
+            // saveToPhotoAlbum: true,
+            sourceType: Camera.PictureSourceType.CAMERA,
+            encodingType: Camera.EncodingType.JPEG,
+            destinationType: Camera.DestinationType.FILE_URI
+          });
+        }else if(index == 1){
+          takePicture({
+            quality : 100,
+            allowEdit : true,
+            targetWidth: 500,
+            targetHeight: 225,
+            sourceType: Camera.PictureSourceType.PHOTOLIBRARY,
+            encodingType: Camera.EncodingType.JPEG,
+            destinationType: Camera.DestinationType.FILE_URI
+          });
+        }else{
+          return true;
+        }
+        hideSheet();
+      }
     });
   };
 
-  $scope.getLocation = function() {
-    log('getLocation()');
+  // upload file with a imageURI
+  var uploadFile = function(imageURI){
+    // show the progress bar
+    safeApply( $scope, function(){
+      $scope.showProgress = true;
+    });
+    var uploadOptions = new FileUploadOptions();
+    uploadOptions.fileKey = "avatar";
+    uploadOptions.fileName = imageURI.substr(imageURI.lastIndexOf('/') + 1);
+    uploadOptions.mimeType = "image/jpeg";
+    uploadOptions.chunkedMode = false;
 
-    // show wait message
-    $scope.gettingLocation = true;
+    var ft = new FileTransfer();
 
-    $cordovaGeolocation.getCurrentPosition($scope.posOptions).then(function(locationData) {
-      log('getCurrentPosition success', locationData);
-      $scope.result = locationData.coords.latitude + ", " + locationData.coords.longitude + " | accuracy: " + locationData.coords.accuracy;
-      log($scope.result);
-      $scope.gettingLocation = false;
-    }, function(err) {
-      log('getCurrentPosition error', err);
-      $scope.result = err;
-      log($scope.result);
-      $scope.gettingLocation = false;
+    var statusDom = document.getElementById("ft-prog");
+
+    ft.onprogress = function(progressEvent) {
+      if (progressEvent.lengthComputable) {
+        var perc = Math.floor(progressEvent.loaded / progressEvent.total * 100);
+        statusDom.value = perc;
+        if(perc == 100){
+          safeApply($scope, function(){
+            $scope.showProgress = false;
+          });
+        }
+      } else {
+        console.log("loading....");
+      }
+    };
+
+    ft.upload(imageURI, encodeURI(uploadFileUrl), onSuccess, onFail, uploadOptions, true);
+
+    function onSuccess(responseData){
+      // FIXME: if use responseData.response.avatar_thumb_url will get a undefined
+      responseString = JSON.stringify(responseData);
+      responseObject = JSON.parse(responseString);
+      responsePerson = JSON.parse(responseObject.response);
+      safeApply($scope, function(){
+        // update your url
+        // $scope.person.avatar_square_url = responsePerson.avatar_square_url;
+      });
+    };
+    function onFail(){
+      alert("something wrong, please try again");
+    };
+  };
+
+  //get photos form device and return a file path url
+  var takePicture = function(options) {
+    CameraPopover.getPicture(options).then(function(imageURI){
+      uploadFile(imageURI);
+    }, function(err){
+      console.error(err);
     });
   };
 
-})
-
-.controller('LogCtrl', function($scope, logService) {
-
-  logService.listen(function(logs) {
-    $scope.logs = logs;
-  });
-
-  logService.log('test log', {
-    field1: 1,
-    field2: 'two'
-  });
-
-  $scope.message = "Log!";
-
-});
+}])
