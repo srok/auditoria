fec360b4a9ef4ac753c316a9d21608e278a9b35e
Rodrigo Barrera <Rodrigo Barrera>
2016-12-13 18:36:42 -0300

    cambio a http porqeu no conecta con https

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-var base_url='https://90.0.0.1/identhya/audit/back-end';
+var base_url='http://90.0.0.1/identhya/audit/back-end';
 
 angular.module('app', ['ionic', 'app.controllers', 'app.routes', 'app.directives','app.services',])
 
